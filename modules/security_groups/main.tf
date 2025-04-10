8678b9d924a9fd942ea23b7c55fcecab5f6a5d34
Kundan547 <kundanvyas197@gmail.com>
2025-04-10 12:17:17 +0530

    Completed

---
+++ modules/security_groups/main.tf
@@ -0,0 +1,104 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Security group for web server"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db-sg"
+  description = "Security group for database server"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "db-sg"
+  }
+}
+
+# Ingress Rules for Web SG
+resource "aws_security_group_rule" "web_ssh_22" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "web_http_80" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "web_app_3000" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "web_backend_5002" {
+  type              = "ingress"
+  from_port         = 5002
+  to_port           = 5002
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "web_allow_mongo_from_db" {
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.db_sg.id
+  security_group_id        = aws_security_group.web_sg.id
+}
+
+# Egress rule for web_sg (allow all outbound)
+resource "aws_security_group_rule" "web_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+}
+
+# Ingress rules for db_sg
+resource "aws_security_group_rule" "db_allow_mongo_from_web" {
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.web_sg.id
+  security_group_id        = aws_security_group.db_sg.id
+}
+
+resource "aws_security_group_rule" "db_ssh_from_web" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.web_sg.id
+  security_group_id        = aws_security_group.db_sg.id
+}
+
+# Egress rule for db_sg (allow all outbound)
+resource "aws_security_group_rule" "db_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.db_sg.id
+}
